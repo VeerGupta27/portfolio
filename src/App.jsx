c5cc56e11686bcb928fbc814660e1dc3d9f7577b
VeerGupta27 <veer27012005@gmail.com>
2026-05-08 17:36:29 +0530

    installed react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
+
 import './App.css'
 
 function App() {
   
 
   return (
-    <>      
+    <>     
+      <Navbar /> 
       <h1>Welcome to my Portfolio</h1>
     </>
   )
