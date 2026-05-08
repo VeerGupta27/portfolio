85c6b08a274602d7363b16408552ee2d10991e51
VeerGupta27 <veer27012005@gmail.com>
2026-05-08 20:59:46 +0530

    added styles to navbar and herosecion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 import './App.css'
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <>     
       <Navbar /> 
-      
+      <HeroSection />
     </>
   )
 }
