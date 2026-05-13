452f804a73134a8923594a734c46fc8b135557f0
VeerGupta27 <veer27012005@gmail.com>
2026-05-13 13:25:23 +0530

    creating About me section, removing skills section, and updating the hero section to be more concise and visually appealing.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,6 @@ export default function Navbar(){
         <div className="links">
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/about" className="nav-link">About</Link>
-        <Link to="/skills" className="nav-link">Skills</Link>
         <Link to="/projects" className="nav-link">Projects</Link>
         <Link to="/contact" className="nav-link">Contact</Link>
         <Link to="/resume" className="nav-link">Resume</Link>
